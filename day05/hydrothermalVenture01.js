9c1ea7e43d60abe7aa1a4ed9057baffb5cf77b3d
Andrés Quilodrán <andres.caro.q@gmail.com>
2022-01-09 23:35:49 -0300

    day05: solve part 01 with Lodash

--- day05/hydrothermalVenture01.js
+++ day05/hydrothermalVenture01.js
@@ -1,5 +1,3 @@
-const inputReader = require("../lib/inputReader");
-
 /*
  * BASE INFO
  * - x1,y1 -> x2,y2 is a vent line
@@ -9,6 +7,7 @@ const inputReader = require("../lib/inputReader");
  */
 
 var _ = require("lodash");
+const inputReader = require("../lib/inputReader");
 
 const X_COORDINATE = "x";
 const Y_COORDINATE = "y";
@@ -23,13 +22,34 @@ function findMax(lines, coordinate) {
   );
 }
 
+/*
+ * Grid display
+ *
+ *  2,  2 -> 2,  1
+ * (x1, y1) (x2, y2)
+ *
+ * (0,0)
+ *   .......1..
+ *   ..1....1..
+ *   ..1....1..
+ *   .......1..
+ *   .112111211
+ *   ..........
+ *   ..........
+ *   ..........
+ *   ..........
+ *   222111....
+ *          (9,9)
+ *
+ */
+
 function buildGrid(width, height) {
   const newGrid = [];
 
-  for (let i = 0; i < width; i += 1) {
+  for (let i = 0; i <= width; i += 1) {
     const newRow = [];
 
-    for (let j = 0; j < height; j += 1) {
+    for (let j = 0; j <= height; j += 1) {
       newRow.push(0);
     }
 
@@ -39,8 +59,73 @@ function buildGrid(width, height) {
   return newGrid;
 }
 
+function addLine(grid, line) {
+  const start = (...arr) => _.min(arr);
+  const end = (...arr) => _.max(arr);
+
+  // Go from start to end adding 1 to each position in the line
+  if (line.x1 === line.x2) {
+    /* Vertical line */
+
+    const first = start(line.y1, line.y2);
+    const last = end(line.y1, line.y2);
+
+    for (let i = first; i <= last; i += 1) {
+      grid[line.x1][i] += 1;
+    }
+  } else if (line.y1 === line.y2) {
+    /* Horizontal line */
+
+    const first = start(line.x1, line.x2);
+    const last = end(line.x1, line.x2);
+
+    for (let i = first; i <= last; i += 1) {
+      grid[i][line.y1] += 1;
+    }
+  } else {
+    // throw new TypeError(
+    //   `Invalid line data (${Object.values(
+    //     line
+    //   )}): either x1 = x2 or y1 = y2 should be true`,
+    //   "hydrothermalVenture01.js",
+    //   50
+    // );
+  }
+}
+
+function addLines(grid, lines) {
+  _.forEach(lines, (line) => {
+    addLine(grid, line);
+  });
+}
+
+function printGrid(grid) {
+  const transposeGrid = (grid) => {
+    return grid[0].map((_, colIndex) => grid.map((row) => row[colIndex]));
+  };
+
+  _.forEach(transposeGrid(grid), (row) => {
+    console.log(row.join("").replaceAll("0", "."));
+  });
+}
+
+function countOverlappingPoints(grid) {
+  // Reduce grid to the sum of all numbers >= 2
+  return _.reduce(
+    grid,
+    (sum, row) => {
+      return _.filter(row, (num) => num >= 2).length + sum;
+    },
+    0
+  );
+}
+
 async function readVentLines(reader) {
   const lines = [];
+  let overlappingPointsCount;
+  let max_x;
+  let max_y;
+  let grid;
 
   for await (const line of reader) {
     const [p1, p2] = line.split(" -> ");
@@ -50,26 +135,31 @@ async function readVentLines(reader) {
     lines.push({ x1, y1, x2, y2 });
   }
 
-  const max_x = findMax(lines, X_COORDINATE);
-  const max_y = findMax(lines, Y_COORDINATE);
+  max_x = findMax(lines, X_COORDINATE);
+  max_y = findMax(lines, Y_COORDINATE);
 
-  const grid = buildGrid(max_x, max_y);
+  grid = buildGrid(max_x, max_y);
 
-  _.forEach(lines, (line) => {});
+  addLines(grid, lines);
+
+  overlappingPointsCount = countOverlappingPoints(grid);
+
+  console.log(overlappingPointsCount);
 }
 
 /*
  * BASE ALGORITHM
- * 1. read all points - check
- * 2. find max_x
- * 3. find max_y
- * 4. build grid of size max_x * max_y
- * 5. foreach point
+ * 1. read all points - ready
+ * 2. find max_x - ready
+ * 3. find max_y - ready
+ * 4. build grid of size max_x * max_y - ready
+ * 5. foreach point - ready
  *    5.1 add line to grid (increase positions included in the line)
- * 6. find amount of points where 2 or more lines overlap
+ * 6. find amount of points where 2 or more lines overlap - ready
  *    - foreach row of grid, count numbers that are >= 2
  *    - add to sum the amount of numbers
- * 7. return sum of numbers >= 2
+ * 7. return sum computed in step 6
+ *
  */
 
 (async () => {
